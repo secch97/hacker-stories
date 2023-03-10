3fe6dd4d13f48baaac7100967498f5599519f509
Saul Castillo <secch97@gmail.com>
2023-03-09 22:30:01 -0500

    React JSX

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-const title = "React";
+const games = ["The Last of Us", "God of War", "Dark Souls"];
 
 function App() {
   return (
     <div>
-      <h1>Hello World! I am using {title}</h1>
+      <h1>List of my top 3 games:</h1>
+      <ol>
+        {
+          games.map((game) => (<li key={game}>{game}</li>))
+        }
+      </ol>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text"/>
     </div>
   );
 }
