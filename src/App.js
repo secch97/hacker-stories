7bca8530fcc26c6ba609ea339263bdf2874ae9fa
Saul Castillo <secch97@gmail.com>
2023-04-05 21:25:00 -0400

    Reusable components in React

--- src/App.js
+++ src/App.js
@@ -66,18 +66,18 @@ const App = () => {
   return (
     <>
       <h1>My Hacker Stories</h1>
-      <Search search={searchTerm} onSearch={handleSearch}/>
+      <InputWithLabel id="search" label="Search: " value={searchTerm} onInputChange={handleSearch}/>
       <hr />
       <List list={searchedStories}/>
     </>
   );
 };
 
-const Search = ({search, onSearch}) => {
+const InputWithLabel = ({id, label, value, type="text", onInputChange}) => {
   return (
     <div>
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" value={search} onChange={onSearch}/>
+      <label htmlFor={id}>{label}</label>
+      <input id={id} type={type} value={value} onChange={onInputChange}/>
     </div>
   );
 };
