3738b63ed89306855cba6939d343f7927380ad35
Saul Castillo <secch97@gmail.com>
2023-04-22 21:07:36 -0400

    Conditional rendering in React

--- src/App.js
+++ src/App.js
@@ -57,10 +57,17 @@ const App = () => {
   */
   const [searchTerm, setSearchTerm] = useSemiPersistentState("search", "React");
   const [stories, setStories] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   useEffect(() => {
+    setIsLoading(true);
+
     getAsyncStories().then((result) => {
       setStories(result.data.stories);
+      setIsLoading(false);
+    }).catch(() => {
+      setIsError(true);
     });
   }, []);
 
@@ -100,7 +107,13 @@ const App = () => {
         <strong>Search:</strong>
       </InputWithLabel>
       <hr />
-      <List list={searchedStories} onRemoveItem={handleRemoveStory} />
+      {isError && (<p>Something went wrong...</p>)}
+      {
+        isLoading ? 
+        (<p>Loading...</p>) : 
+        (<List list={searchedStories} onRemoveItem={handleRemoveStory} />)
+      }
+
     </>
   );
 };
